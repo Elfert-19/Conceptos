6631745d9d98785e3822f97c07035ff8c41623f0
Elfert-19 <rcg1990@gmail.com>
2018-03-22 23:07:27 -0300

    Bala de rifle de asalto.

--- Assets/Concepto 1/Scipts/Weapon/AssaultRiffleBullet.cs
+++ Assets/Concepto 1/Scipts/Weapon/AssaultRiffleBullet.cs
@@ -2,15 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AssaultRiffleBullet : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class AssaultRiffleBullet : Bullet {
+    
 }
