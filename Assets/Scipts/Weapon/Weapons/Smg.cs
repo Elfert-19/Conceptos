5d807534fc65ae9058bddcbd8caec94af680bdd3
Elfert-19 <rcg1990@gmail.com>
2018-03-29 20:56:23 -0300

    Primera version SMG con su respectiva balla.

---
+++ Assets/Scipts/Weapon/Weapons/Smg.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smg : Weapon {
+
+    [SerializeField]
+    float maxDistance;
+    [SerializeField]
+    LayerMask layer;
+    float shootTime;
+    bool hitMiss = true;
+
+    // Disparo primario del Assault Rifle
+    public override void PrimaryFire()
+    {
+        Vector3 miss = new Vector3(0, -200, 0);
+        hitMiss = true;
+        RaycastHit hitInfo;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hitInfo, maxDistance, layer))
+        {
+            Vector3 inpactPoint = Vector3.Normalize(hitInfo.point - firePoint.position);
+
+            if (Physics.Raycast(firePoint.position, inpactPoint, out hitInfo, maxDistance, layer))
+            {
+                Vector3 shotPoint = hitInfo.point;
+                RapidFire(shotPoint);
+                hitMiss = false;
+            }
+        }
+        if (hitMiss)
+        {
+            RapidFire(miss);
+        }
+    }
+    // Se encarga de disparar de manera continua y controlar la cadencia de fuego
+    public void RapidFire(Vector3 position)
+    {
+        float timeBetweenBullets = cadence / magazine;
+        if (currentMagazine > 0)
+        {
+            if (Time.time - shootTime > timeBetweenBullets)
+            {
+                shootTime = Time.time;
+                FireBullet(position);
+            }
+        }
+    }
+}
